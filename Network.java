c27990db7c2579f12ae65ae281a74755a750dba0
unknown <boaz6151@gmail.com>
2025-01-09 12:50:13 +0200

    Comp

--- Network.java
+++ Network.java
@@ -40,9 +40,9 @@ public class Network {
      * Notice that the method receives a String, and returns a User object.
      */
     public User getUser(String name) {
-        for (User user : users) {
-            if (user.getName().equals(name)) {
-                return user;
+        for (int i = 0; i < userCount; i++) {
+            if (users[i] != null && users[i].getName().equals(name)) {
+                return users[i];
             }
         }
         return null;
